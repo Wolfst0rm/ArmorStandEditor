41acb4857bd99598f58f07fccb5b92348381d9de
Wolfieheart <xmakeitrain94@gmail.com>
2023-04-15 20:34:22 +0200

    [API] Fix for Implements Cancellable
    Signed-off-by: Wolfieheart <xmakeitrain94@gmail.com>

--- src/main/java/io/github/rypofalem/armorstandeditor/api/ArmorStandManipulatedEvent.java
+++ src/main/java/io/github/rypofalem/armorstandeditor/api/ArmorStandManipulatedEvent.java
@@ -1,12 +1,17 @@
 package io.github.rypofalem.armorstandeditor.api;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 
 public class ArmorStandManipulatedEvent extends ArmorStandEvent implements Cancellable {
+
+    @Getter @Setter
+    private boolean cancelled = false;
+
     @Getter
     protected final Player player;
 
@@ -19,6 +24,4 @@ public class ArmorStandManipulatedEvent extends ArmorStandEvent implements Cance
     private static final HandlerList handlers = new HandlerList();
     public static HandlerList getHandlerList() { return (handlers); }
     @Override public HandlerList getHandlers() { return (handlers); }
-    @Override public boolean isCancelled() {return false;}
-    @Override public void setCancelled(boolean b) { }
 }
