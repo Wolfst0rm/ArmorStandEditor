4e2ec5e23b2fba5219672a19997d9591154d7b9b
Wolfieheart <xmakeitrain94@gmail.com>
2025-01-03 23:14:01 +0100

    [CORE] Remove EditorMenu for now. Fix ASE Holder ;)
    Signed-off-by: Wolfieheart <xmakeitrain94@gmail.com>

--- src/main/java/io/github/rypofalem/armorstandeditor/menu/ASEHolder.java
+++ src/main/java/io/github/rypofalem/armorstandeditor/menu/ASEHolder.java
@@ -3,5 +3,9 @@ package io.github.rypofalem.armorstandeditor.menu;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.Inventory;
 
-public interface ASEHolder extends InventoryHolder {
+public class ASEHolder implements InventoryHolder {
+    @Override
+    public Inventory getInventory() {
+        return null;
+    }
 }
--- src/main/java/io/github/rypofalem/armorstandeditor/menu/EditorMenu.java
+++
@@ -1,8 +0,0 @@
-package io.github.rypofalem.armorstandeditor.menu;
-
-import org.bukkit.entity.ArmorStand;
-
-public interface EditorMenu extends ASEHolder{
-    void open();
-    ArmorStand getArmorStand();
-}
